62c6967f4ff46f167b4011646ac80694fc5de49d
Uladzimir Zylevich <zylevich_uu@zylevichUU.itd.iba.by>
2019-07-20 19:03:02 +0300

    new branch creation

--- src/com/father/example/main/Main.java
+++ src/com/father/example/main/Main.java
@@ -2,7 +2,7 @@ package com.father.example.main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello father commit 2");
+        System.out.println("Hello father commit 2 branch 1.0.0");
     }
 
 }
